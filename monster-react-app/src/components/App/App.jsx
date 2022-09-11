d59cac7c602f71a1e7d7bc8c303fe8e7e88ae24d
André Luiz Morais <andreluiz.morais19@gmail.com>
2022-09-11 18:48:59 -0300

    Optimizations

--- monster-react-app/src/components/App/App.jsx
+++ monster-react-app/src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import CardList from '../card-list/cardList';
 import './App.css'
 
 const url = 'https://jsonplaceholder.typicode.com/users' // API 
@@ -9,8 +10,10 @@ class App extends Component {
     super();
 
     this.state = {
-      monster: []
-    }
+      monster: [],
+      searchInptValue: ''
+    };
+
 
   }
 
@@ -22,35 +25,41 @@ class App extends Component {
       }))
   }
 
+  onSearchChange = event => {
+    const searchInptValue = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchInptValue }
+    });
+  }
+
   render() {
+    //Optimization
+    const { monster, searchInptValue } = this.state;
+    const { onSearchChange } = this
+
+    // imbutabilidade
+    // nao modificar dados, e sim criar novos (uma var pra .filter)
+    const filtrados = monster.filter(monster =>
+      monster.name.toLowerCase().includes(searchInptValue)
+    );
+
     return (
       <div className="App">
         <input
           className="search-box"
           placeholder='Search'
           type="search"
-          onChange={e => {
-            //.includes() é não CaseSensitive [uma var pra ser tudo minúsculo na busca]
-            const searchInptValue = e.target.value.toLowerCase();
-
-            const filtrados = this.state.monster.filter(monster =>
-              monster.name.toLowerCase().includes(searchInptValue)
-            );
-            // imbutabilidade
-            // nao modificar dados, e sim criar novos (uma var pra .filter)
-            this.setState(() => {
-              return { monster: filtrados }
-            });
-          }}
+          onChange={onSearchChange}
         />
-
-        {this.state.monster.map((monster) => {
+        <CardList monster={'props de monster'} />
+{/* 
+        {filtrados.map((monster) => {
           return (
             <div key={monster.id}>
               <h3> {monster.name} </h3>
             </div>
           )
-        })}
+        })} */}
       </div>
     )
   }
---
+++ monster-react-app/src/components/card-list/cardList.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class CardList extends Component {
+
+  render() {
+
+    console.log(this.props)
+
+    return (
+      <h1> Card List teste </h1>
+    );
+  }
+
+}
+
+export default CardList;
